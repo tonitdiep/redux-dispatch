3af8e3f5da242736dd5a2c48bdcb7044151f1ff0
tonitdiep <adiep123@gmail.com>
2021-05-26 22:52:55 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,60 @@
+// function changeState(state, action){
+  
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return {count: state.count + 1}
+//     default:
+//       return state;
+//   }
+// }
+
+// let state = {count: 0}
+// let action = {type: 'INCREASE_COUNT'}
+
+// changeState(state, action)
+
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function render(){
+    document.body.textContent = state.count
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function dispatch(action){
+  state = changeState(state, action)
+  render()
+}
 
-changeState(state, action)
+render() 
+
+
+// let state = {count: 0};
+
+// function changeState(state, action){
+//     switch (action.type) {
+//       case 'INCREASE_COUNT':
+//         return {count: state.count + 1}
+//       default:
+//         return state;
+//     }
+//   }
+
+// function dispatch(action){
+//   state = changeState(state, action)
+//   return state
+// }
+
+// function render(){
+//   document.body.textContent = state.count
+// }
+
+// // call the render function
+// render() 
